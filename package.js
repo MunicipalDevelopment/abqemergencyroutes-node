6d32499d3eb157391d3a6729790264ed4aaccd8b
City of Albuquerque <pcrickard@cabq.gov>
2016-01-20 14:54:32 -0700

    Update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 {
   "name": "abqemergencyroutes",
-  "version": "1.0.7",
+  "version": "1.0.8",
   "description": "Returns all the emergency routes in ABQ as JSON",
   "main": "routes.js",
   "scripts": {
