a2ebc99d5e8f324908ddf0bfbb21f80b9cd8b118
City of Albuquerque <pcrickard@cabq.gov>
2016-01-20 15:17:04 -0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ getAllEmergencyRoutes:function(callback){
       url = "http://gisdmd.cabq.gov/dmdview/rest/services/public/SurveyMonuments/MapServer/0/query?where=1=1&outFields=*&outSR=4326&f=json",
       route;
 
-// Code snippet from http://stackoverflow.com/questions/16148403/using-node-js-to-connect-to-a-rest-api Robert Mitchell
+
 return http.get(url,function (response) {
 
     response.on("data", function (chunk) {
