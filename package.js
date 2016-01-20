f556a42b38978aca8c1d2cf9828a6aec0249fa42
City of Albuquerque <pcrickard@cabq.gov>
2016-01-20 13:14:11 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+  "name": "abqemergencyroutes",
+  "version": "1.0.2",
+  "description": "Returns all the emergency routes in ABQ as JSON",
+  "main": "routes.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "http://github.com/municipaldevelopment/abqemergencyroutes-node/"
+  },
+  "keywords": [
+    "abq",
+    "albuquerque",
+    "dmd",
+    "municipal",
+    "development",
+    "emergency",
+    "routes"
+  ],
+  "author": "Paul Crickard",
+  "license": "MIT"
+}
